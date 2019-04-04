9309b5ef248738c3294396580d5679b405ab744c
Neil Charlton <neil.charlton@extreme-creations.co.uk>
2019-04-04 16:05:12 +0100

    First Commit

---
+++ backend.php
@@ -0,0 +1,15 @@
+<?php // Check if form was submitted:
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['recaptcha_response'])) {
+
+    // Build POST request:
+    $recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
+    $recaptcha_secret = '6LfO0JsUAAAAAI0n85zL6qLGT0XpHWnwOXa8A5Yj';
+    $recaptcha_response = $_GET['recaptcha_response'];
+
+    // Make and decode POST request:
+    $recaptcha = file_get_contents($recaptcha_url . '?secret=' . $recaptcha_secret . '&response=' . $recaptcha_response);
+    $recaptcha = json_decode($recaptcha);
+
+    echo json_encode($recaptcha);
+   
+} ?>
